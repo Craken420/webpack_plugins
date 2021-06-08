652645d0c76fff7352ffb6ebb0abbd4a00299f50
Luis Angel Peña <lapena@mavi.mx>
2021-06-08 10:43:47 -0500

    feat:: Raed the images with webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+
 import passwordIcon from './img/lock-solid.svg';
 import copyIcon from './img/copy-solid.svg';
     
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,29 @@ const webpackConfig = {
                 test: /\.css$/i,
                 exclude: /node_modules/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10 * 1024, // Images larger than 10 KB won’t be inlined
+                        filename: 'images/[hash]-[name].[ext]',
+                        publicPath: 'assets'
+                    }
+                }]
+            },
+            {
+                test: /\.svg$/i,
+                use:[{
+                    loader: 'svg-url-loader',
+                    options: {
+                        limit: 10 * 1024, // Images larger than 10 KB won’t be inlined
+                        // Remove quotes around the encoded URL –
+                        // they’re rarely useful
+                        noquotes: true
+                    }
+                }]
             }
         ]
     }
